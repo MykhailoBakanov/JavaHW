3b53c3f2fba1ef0327613ea2b8cc06c2f33c4204
MykhailoBakanov <117221737+MykhailoBakanov@users.noreply.github.com>
2022-11-17 21:29:22 +0100

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,30 @@
+public class homework {
+
+    public static void main(String[] args) {
+        System.out.println("First level:");
+        char c1 = 'G';
+        int x = 89;
+        byte y = 4;
+        short z = 56;
+        float q = 4.7333436f;
+        double p = 4.355453532;
+        long k = 12121;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(q);
+        System.out.println(p);
+        System.out.println(k);
+        System.out.println(c1);
+        System.out.println("Second level:");
+        int Num = 150;
+        int s = Num /100;
+        int d = Num / 10 % 10;
+        int e = Num % 10;
+        System.out.println(Num);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(e);
+    }
+
+}
